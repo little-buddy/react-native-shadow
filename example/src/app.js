c5b3d99b844df9e2a0b4bb6e996e2807152a6639
little-buddy <litoubuddy@gmail.com>
2019-08-18 14:43:50 +0800

    refactor: boxShadow 自动测量大小

---
+++ example/src/app.js
@@ -0,0 +1,36 @@
+/**
+ * Created by xiaoxing on 2019-08-16.
+ */
+
+import React from 'react';
+import { View, SafeAreaView, Text } from 'react-native';
+import BoxShadow from './BoxShadow';
+
+/*
+  对这个库进行一些修改吧
+* * */
+
+export default (props: any) => (
+  <SafeAreaView>
+    <Text>shadow Demo</Text>
+    <View
+      style={{
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <BoxShadow
+        setting={{
+          y: 10,
+          opacity: 0.4,
+          border: 1,
+          radius: 12,
+          color: '#ff8080',
+          inset: true,
+        }}>
+        <View style={{ backgroundColor: 'red', borderRadius: 12 }}>
+          <Text style={{ paddingHorizontal: 12 }}>boxShadow</Text>
+        </View>
+      </BoxShadow>
+    </View>
+  </SafeAreaView>
+);
